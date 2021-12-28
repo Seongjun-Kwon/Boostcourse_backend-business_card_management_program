3337362b45c2ff5129b848075fa0970c8d6081da
Kwon seong jun <tjdwns1865@naver.com>
2021-12-28 14:03:14 +0900

    프로젝트 생성

---
+++ src/test/java/boostcourse/backend/cardmanager/CardmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package boostcourse.backend.cardmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
