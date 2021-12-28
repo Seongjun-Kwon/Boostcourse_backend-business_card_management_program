f580b43998e3e1e523ac6599739748da078ca876
Kwon seong jun <tjdwns1865@naver.com>
2021-12-28 15:10:07 +0900

    skeleton code

---
+++ src/main/java/boostcourse/backend/cardmanager/CardManager.java
@@ -0,0 +1,37 @@
+package boostcourse.backend.cardmanager;
+
+import boostcourse.backend.cardmanager.dao.BusinessCardManagerDao;
+import boostcourse.backend.cardmanager.dto.BusinessCard;
+import boostcourse.backend.cardmanager.ui.CardManagerUI;
+
+import java.util.List;
+
+public class CardManager {
+    public static void main(String[] args){
+        CardManagerUI cardManagerUI = CardManagerUI.getInstance();
+        BusinessCardManagerDao businessCardManagerDao = new BusinessCardManagerDao();
+
+        while_loop:
+        while(true) {
+            cardManagerUI.printMainMenu();
+            int menuNumber = cardManagerUI.getMenuNumber();
+            switch(menuNumber){
+                case 1 :
+                    BusinessCard businessCard = cardManagerUI.inputBusinessCard();
+                    businessCardManagerDao.addBusinessCard(businessCard);
+                    break;
+                case 2 :
+                    String searchKeyword = cardManagerUI.getSearchKeyword();
+                    List<BusinessCard> businessCardList = businessCardManagerDao.searchBusinessCard(searchKeyword);
+                    cardManagerUI.printBusinessCards(businessCardList);
+                    break;
+                case 3 :
+                    cardManagerUI.printExitMessage();
+                    break while_loop;
+                default:
+                    cardManagerUI.printErrorMessage();
+                    break;
+            }
+        }
+    }
+}
---
+++ src/main/java/boostcourse/backend/cardmanager/dao/BusinessCardManagerDao.java
@@ -0,0 +1,15 @@
+package boostcourse.backend.cardmanager.dao;
+
+import boostcourse.backend.cardmanager.dto.BusinessCard;
+
+import java.util.List;
+
+public class BusinessCardManagerDao {
+    public List<BusinessCard> searchBusinessCard(String keyword){
+        // 구현하세요.
+    }
+
+    public BusinessCard addBusinessCard(BusinessCard businessCard){
+        // 구현하세요.
+    }
+}
